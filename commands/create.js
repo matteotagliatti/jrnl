f90d8422f2c57594ddb6563cf18a228166281912
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-02-09 10:31:00 +0100

    check dates and create file

--- commands/create.js
+++ commands/create.js
@@ -3,4 +3,26 @@ import fs from "fs";
 const homedir = os.homedir();
 const foldername = ".journal";
 
-export function create() {}
+export function create() {
+  if (!fs.existsSync(`${homedir}/${foldername}`)) {
+    fs.mkdirSync(`${homedir}/${foldername}`);
+  }
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const filename = `${day}.md`;
+
+  if (!fs.existsSync(`${homedir}/${foldername}/${year}`)) {
+    fs.mkdirSync(`${homedir}/${foldername}/${year}`);
+  }
+
+  if (!fs.existsSync(`${homedir}/${foldername}/${year}/${month}`)) {
+    fs.mkdirSync(`${homedir}/${foldername}/${year}/${month}`);
+  }
+
+  if (!fs.existsSync(`${homedir}/${foldername}/${year}/${month}/${filename}`)) {
+    fs.writeFileSync(`${homedir}/${foldername}/${year}/${month}/${filename}`);
+  }
+}
