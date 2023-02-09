362d01fa9303efd2255fd7491166ae2fa10e3af4
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-02-09 11:10:06 +0100

    create/appen today entry

--- commands/create.js
+++ commands/create.js
@@ -2,6 +2,13 @@ import os from "os";
 import fs from "fs";
 const homedir = os.homedir();
 const foldername = ".journal";
+const separator = "---";
+
+import { createInterface } from "readline";
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 export function create() {
   if (!fs.existsSync(`${homedir}/${foldername}`)) {
@@ -22,7 +29,17 @@ export function create() {
     fs.mkdirSync(`${homedir}/${foldername}/${year}/${month}`);
   }
 
-  if (!fs.existsSync(`${homedir}/${foldername}/${year}/${month}/${filename}`)) {
-    fs.writeFileSync(`${homedir}/${foldername}/${year}/${month}/${filename}`);
+  const todayFile = `${homedir}/${foldername}/${year}/${month}/${filename}`;
+  if (!fs.existsSync(todayFile)) {
+    fs.writeFileSync(todayFile, "", { overwrite: false });
+    readline.question(`Write today entry ${separator} `, (answer) => {
+      fs.appendFileSync(todayFile, `${answer}`);
+      readline.close();
+    });
+  } else {
+    readline.question(`Append today entry ${separator} `, (answer) => {
+      fs.appendFileSync(todayFile, `\r\n\r\n${answer}`);
+      readline.close();
+    });
   }
 }
