0e22594f4a1631ffeec82134b44090e0cd2158cf
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-02-09 12:27:23 +0100

    view complete

--- commands/view.js
+++ commands/view.js
@@ -1,9 +1,9 @@
-import { fs, todayFile } from "../utils/shared.js";
+import { fs, todayFile, homedir, foldername } from "../utils/shared.js";
 
 export function view(date) {
   if (date === undefined) {
     console.log("Please provide a date");
-    return;
+    process.exit();
   }
 
   if (date === "today") {
@@ -12,6 +12,38 @@ export function view(date) {
     } else {
       console.log("No entry for today");
     }
-    return;
+
+    process.exit();
   }
+
+  const year = date.split("-")[0];
+  let month = date.split("-")[1];
+  let day = date.split("-")[2];
+
+  if (fs.existsSync(`${homedir}/${foldername}/${year}`)) {
+    console.log("Directory with this year exists");
+    month = month[0] === "0" ? month[1] : month;
+    if (fs.existsSync(`${homedir}/${foldername}/${year}/${month}`)) {
+      day = day[0] === "0" ? day[1] : day;
+
+      if (
+        fs.existsSync(`${homedir}/${foldername}/${year}/${month}/${day}.md`)
+      ) {
+        console.log(
+          fs.readFileSync(
+            `${homedir}/${foldername}/${year}/${month}/${day}.md`,
+            "utf8"
+          )
+        );
+      } else {
+        console.log("No entry for this date");
+      }
+    } else {
+      console.log("No directory with this month");
+    }
+  } else {
+    console.log("No directory with this year");
+  }
+
+  process.exit();
 }
